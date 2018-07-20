500b87f1f4219866f9456acb8a0c612494ff24bd
marknotbatman <30682747+marknotbatman@users.noreply.github.com>
2018-07-20 14:02:40 -0500

    Made Client Requested Updates

--- wp-content/themes/mama/header.php
+++ wp-content/themes/mama/header.php
@@ -16,20 +16,20 @@
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<meta property="og:title" content="Mama Film">
-		<meta property="og:description" content="Earlybird registration starts May 20th">
-		<meta property="og:url" content="http://com" />
+		<meta property="og:description" content="<?php bloginfo('description'); ?>">
+		<meta property="og:url" content="http://mama.film" />
 		<meta property="og:type" content="website" />
-		<meta property="og:image" content="http://.com//mama-2018-social-share.jpg" />
+		<meta property="og:image" content="<?php the_field('logo') ?>" />
 
-		<meta name="twitter:site" value="@CreativeRushKS" />
-		<meta property="twitter:url" content="http://.com" />
+		<meta name="twitter:site" value="@MamaFilm1" />
+		<meta property="twitter:url" content="https://twitter.com/MamaFilm1" />
 		<meta property="twitter:title" content="Mama Film" />
-		<meta property="twitter:description" content="Earlybird registration starts May 20th" />
-		<meta property="twitter:image" content="http://.com/mama-2018-social-share.jpg" />
+		<meta property="twitter:description" content="<?php bloginfo('description'); ?>" />
+		<meta property="twitter:image" content="<?php the_field('logo') ?>" />
 		<meta name="twitter:card" value="summary_large_image" />
 
 		<link rel="stylesheet" href="/wp-content/themes/mama/assets/dist/css/app.css">
-		<link rel="shortcut icon" href="/mama-favicon.png">
+		<!-- <link rel="shortcut icon" href="/mama-favicon.png"> -->
 		<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
 
 		<script>
@@ -48,6 +48,27 @@
 
 		<header class="" role="banner">
 			<div class="row">
-
+				<div class="columns medium-10 medium-offset-1">
+					<ul class="social right">
+						<li>
+							<a class="social-link instagram" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/mamafilm1/">
+								<i class="fab fa-instagram"></i>
+								<span>Instagram</span>
+							</a>
+						</li>	
+						<li>
+							<a class="social-link facebook" target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/MamaFilmFest/">
+								<i class="fab fa-facebook-square"></i>
+								<span>Facebook</span>
+							</a>
+						</li>	
+						<li>
+							<a class="social-link twitter" target="_blank" rel="noopener noreferrer" href="https://twitter.com/MamaFilm1">
+								<i class="fab fa-twitter-square"></i>
+								<span>Twitter</span>
+							</a>
+						</li>	
+					</ul>
+				</div>
 			</div>					
 		</header>
